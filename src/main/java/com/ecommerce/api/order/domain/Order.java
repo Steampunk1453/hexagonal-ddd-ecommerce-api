31cd52e399141350254b76abd985e1c900871041
Steampunk <dasies11@gmail.com>
2020-12-23 23:47:07 +0100

    Add business rules to aggregate

--- src/main/java/com/ecommerce/api/order/domain/Order.java
+++ src/main/java/com/ecommerce/api/order/domain/Order.java
@@ -18,8 +18,8 @@ public class Order {
 
     private BigDecimal price;
 
-    public Order(UUID randomUUID, Product product) {
-        id = randomUUID;
+    public Order(final UUID id, final Product product) {
+        this.id = id;
         this.orderItems = new ArrayList<>(Collections.singletonList(new OrderItem(product)));;
         this.product = product;
         this.price =  product.value().getNumberStripped();
@@ -33,6 +33,39 @@ public class Order {
         return product;
     }
 
-    public void addProduct(final Product product) {
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
     }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void addProduct(final Product product) {
+        validateProduct(product);
+        orderItems.add(new OrderItem(product));
+        price = price.add(product.value().getNumberStripped());
+    }
+
+    public void remove(final UUID id) {
+        final OrderItem orderItem = getOrderItem(id);
+        orderItems.remove(orderItem);
+        price = price.subtract(orderItem.product().value().getNumberStripped());
+    }
+
+    private OrderItem getOrderItem(final UUID id) {
+        return orderItems.stream()
+            .filter(orderItem -> orderItem.product().id()
+                .equals(id))
+            .findFirst()
+            .orElseThrow(() -> new DomainException("Product with " + id + " doesn't exist."));
+    }
+
+    private void validateProduct(final Product product) {
+        if (product == null) {
+            throw new DomainException("The product cannot be null");
+        }
+    }
+
+
 }
