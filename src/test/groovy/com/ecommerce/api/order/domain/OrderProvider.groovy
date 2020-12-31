4a544d57e2ba5fa57e6c239b5c3b3baf6f124a74
Steampunk <dasies11@gmail.com>
2020-12-31 11:51:33 +0100

    Use to build orders in the use cases and domain tests

---
+++ src/test/groovy/com/ecommerce/api/order/domain/OrderProvider.groovy
@@ -0,0 +1,19 @@
+package com.ecommerce.api.order.domain
+
+import org.javamoney.moneta.Money
+
+import com.ecommerce.api.order.domain.model.Order
+import com.ecommerce.api.order.domain.model.Product
+
+class OrderProvider {
+
+    static Order buildOrder() {
+        UUID orderId = UUID.randomUUID()
+        UUID productId = UUID.randomUUID()
+        Money value = Money.of(new BigDecimal(2.50), "EUR")
+        Product product = new Product(productId, "product", value)
+        Integer productQuantity = 1
+        new Order(orderId, product, productQuantity)
+    }
+
+}
