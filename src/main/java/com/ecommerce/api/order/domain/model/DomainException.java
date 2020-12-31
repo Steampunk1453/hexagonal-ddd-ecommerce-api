2a709d84127b018318c2ef4c9089cd41104c847f
Steampunk <dasies11@gmail.com>
2020-12-31 11:50:42 +0100

    Open class

--- src/main/java/com/ecommerce/api/order/domain/model/DomainException.java
+++ src/main/java/com/ecommerce/api/order/domain/model/DomainException.java
@@ -1,6 +1,6 @@
 package com.ecommerce.api.order.domain.model;
 
-class DomainException extends RuntimeException {
+public class DomainException extends RuntimeException {
     DomainException(final String message) {
         super(message);
     }
