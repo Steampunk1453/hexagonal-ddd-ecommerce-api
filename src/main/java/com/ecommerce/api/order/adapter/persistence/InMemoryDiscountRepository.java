7732d49a872669b1683eb6d83550b2c3f7427de7
Steampunk <dasies11@gmail.com>
2021-01-07 23:51:37 +0100

    Add more type of discounts

--- src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryDiscountRepository.java
+++ src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryDiscountRepository.java
@@ -1,12 +1,15 @@
-package com.ecommerce.api.order.domain.port;
+package com.ecommerce.api.order.adapter.persistence;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.ecommerce.api.order.application.discount.FinancialDiscountStrategy;
 import com.ecommerce.api.order.application.discount.MarketingDiscountStrategy;
 import com.ecommerce.api.order.domain.model.discount.Discount;
+import com.ecommerce.api.order.domain.port.DiscountRepository;
 
 @Component
 public class InMemoryDiscountRepository implements DiscountRepository {
@@ -14,9 +17,9 @@ public class InMemoryDiscountRepository implements DiscountRepository {
     private final Map<String, Discount> discounts = new HashMap<>();
 
     @Override
-    public void save(Discount discount) {
-        MarketingDiscountStrategy marketingDiscountStrategy = new MarketingDiscountStrategy();
-        discounts.put("STICKER", new Discount("STICKER", true, marketingDiscountStrategy));
+    public void save(Discount discountParameters) {
+        discounts.put("STICKER", new Discount(true, new MarketingDiscountStrategy(2)));
+        discounts.put("TSHIRT",  new Discount(true, new FinancialDiscountStrategy(3, new BigDecimal(1))));
     }
 
     @Override
