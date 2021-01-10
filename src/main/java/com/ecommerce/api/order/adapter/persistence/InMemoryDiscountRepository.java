606769c7a42c59d051bf4d7b623adc36a2780aa6
Steampunk <dasies11@gmail.com>
2021-01-10 20:48:21 +0100

    Update repository with new logic and methods

--- src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryDiscountRepository.java
+++ src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryDiscountRepository.java
@@ -1,13 +1,10 @@
 package com.ecommerce.api.order.adapter.persistence;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-import com.ecommerce.api.order.application.discount.FinancialDiscountStrategy;
-import com.ecommerce.api.order.application.discount.MarketingDiscountStrategy;
 import com.ecommerce.api.order.domain.model.discount.Discount;
 import com.ecommerce.api.order.domain.port.DiscountRepository;
 
@@ -17,14 +14,23 @@ public class InMemoryDiscountRepository implements DiscountRepository {
     private final Map<String, Discount> discounts = new HashMap<>();
 
     @Override
-    public void save(Discount discountParameters) {
-        discounts.put("STICKER", new Discount(true, new MarketingDiscountStrategy(2)));
-        discounts.put("TSHIRT",  new Discount(true, new FinancialDiscountStrategy(3, new BigDecimal(1))));
+    public void save(String discountCode, Discount discount) {
+        discounts.put(discountCode, discount);
+    }
+
+    @Override
+    public Map<String, Discount> getAll() {
+        return discounts;
     }
 
     @Override
     public Discount get(String code) {
-        return discounts.get("STICKER");
+        return discounts.get(code);
+    }
+
+    @Override
+    public void delete(String code) {
+        discounts.remove(code);
     }
 
 }
