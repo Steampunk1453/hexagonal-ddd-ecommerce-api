81cc71499ad8ad2665150b3e7560732848888ea0
Steampunk <dasies11@gmail.com>
2021-01-31 23:57:47 +0100

    Upgrade quantity

--- src/main/java/com/ecommerce/api/order/domain/model/OrderItem.java
+++ src/main/java/com/ecommerce/api/order/domain/model/OrderItem.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 import javax.validation.constraints.Min;
 
-public record OrderItem(Product product, @Min(value = 0, message = "Quantity must be greater than zero") Integer quantity, BigDecimal price) {
+public record OrderItem(Product product, @Min(value = 1, message = "Quantity must be greater than zero") Integer quantity, BigDecimal price) {
 
 }
 
