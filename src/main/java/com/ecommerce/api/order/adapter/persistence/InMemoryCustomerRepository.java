75671c82786997bb870746dd3bb259b1dad98d6d
Steampunk <dasies11@gmail.com>
2021-01-31 22:04:43 +0100

    Update methods to save data in a map

--- src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryCustomerRepository.java
+++ src/main/java/com/ecommerce/api/order/adapter/persistence/InMemoryCustomerRepository.java
@@ -1,5 +1,7 @@
 package com.ecommerce.api.order.adapter.persistence;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,14 +13,16 @@ import com.ecommerce.api.order.domain.port.CustomerRepository;
 @Component
 public class InMemoryCustomerRepository implements CustomerRepository {
 
+    private final Map<UUID, Customer> customers = new HashMap<>();
+
     @Override
     public void save(Customer customer) {
-
+        customers.put(customer.id(), customer);
     }
 
     @Override
     public Optional<Customer> findById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(customers.get(id));
     }
 
 }
