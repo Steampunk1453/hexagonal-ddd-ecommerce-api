0f8242e1b50ff592a4db0fe4c5a20d36ae6168bc
Steampunk <dasies11@gmail.com>
2021-02-15 20:11:39 +0100

    Add modifier to have immutable param and remove validation

--- src/main/java/com/ecommerce/api/order/domain/model/OrderItem.java
+++ src/main/java/com/ecommerce/api/order/domain/model/OrderItem.java
@@ -2,9 +2,7 @@ package com.ecommerce.api.order.domain.model;
 
 import java.math.BigDecimal;
 
-import javax.validation.constraints.Min;
-
-public record OrderItem(Product product, @Min(value = 1, message = "Quantity must be greater than zero") Integer quantity, BigDecimal price) {
+public record OrderItem(Product product, Integer quantity, BigDecimal price) {
 
 }
 
