595d234a0aad09c94e7dd7a5daaa3200a5bc18f8
Steampunk <dasies11@gmail.com>
2021-03-14 22:08:53 +0100

    Update to map request to domain

--- src/main/java/com/ecommerce/api/order/adapter/web/CreateCustomerController.java
+++ src/main/java/com/ecommerce/api/order/adapter/web/CreateCustomerController.java
@@ -1,16 +1,16 @@
 package com.ecommerce.api.order.adapter.web;
 
-import java.util.UUID;
-
+import com.ecommerce.api.order.adapter.web.request.CreateCustomerRequest;
+import com.ecommerce.api.order.application.usecase.CreateCustomer;
+import com.ecommerce.api.order.domain.model.customer.Address;
+import com.ecommerce.api.order.domain.model.customer.PersonalData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecommerce.api.order.adapter.web.request.CreateCustomerRequest;
-import com.ecommerce.api.order.application.usecase.CreateCustomer;
-import com.ecommerce.api.order.domain.model.customer.Address;
+import java.util.UUID;
 
 @RestController
 public class CreateCustomerController {
@@ -23,7 +23,7 @@ public class CreateCustomerController {
 
     @PostMapping("/customers")
     ResponseEntity<UUID> create(@RequestBody final CreateCustomerRequest request) {
-        final var customerId = createCustomer.execute(request.personalData(), Address.toDomain(request.address()), request.orderId());
+        final var customerId = createCustomer.execute(PersonalData.toDomain(request.personalData()), Address.toDomain(request.address()), request.orderId());
         return new ResponseEntity<>(customerId, HttpStatus.CREATED);
     }
 
--- src/main/java/com/ecommerce/api/order/domain/model/customer/PersonalData.java
+++ src/main/java/com/ecommerce/api/order/domain/model/customer/PersonalData.java
@@ -1,7 +1,13 @@
 package com.ecommerce.api.order.domain.model.customer;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ecommerce.api.order.adapter.web.request.PersonalDataRequest;
 
-public record PersonalData(@JsonProperty String name, @JsonProperty String surname) {
+public record PersonalData(String name, String surname) {
+
+    public static PersonalData toDomain(final PersonalDataRequest request) {
+        return new PersonalData(
+                request.name(),
+                request.surname());
+    }
 
 }
